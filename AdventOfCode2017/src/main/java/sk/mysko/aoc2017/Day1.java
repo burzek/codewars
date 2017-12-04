c45770cd9aee7f30ed96fd95c89999fa7605349f
boris.brinza <boris.brinza@aevi.cz>
2017-12-04 11:45:48 +0100

    aoc 2017 day 1

---
+++ AdventOfCode2017/src/main/java/sk/mysko/aoc2017/Day1.java
@@ -0,0 +1,31 @@
+package sk.mysko.aoc2017;
+
+/**
+ * @author boris.brinza 04-Dec-2017.
+ */
+public class Day1 extends AdventOfCodeBase<Integer> {
+
+	public static void main(String[] args) {
+		Day1 day1 = new Day1();
+		String input = day1.readFile("/Day1.input");
+		System.out.println("result:" + day1.runPart1(input));
+
+
+
+	}
+
+	@Override
+	protected Integer runPart1(String input) {
+		int sum = 0;
+		for (int i = 0; i < input.length(); i++) {
+			int next = (i + 1 == input.length()) ? 0 : i + 1;
+			sum += (input.charAt(i) == input.charAt(next) ? (input.charAt(i) - '0') : 0);
+		}
+		return sum;
+	}
+
+	@Override
+	protected Integer runPart2(String input) {
+		return null;
+	}
+}
