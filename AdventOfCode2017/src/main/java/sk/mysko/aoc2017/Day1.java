985a7f9ce30d2b604006bc938f7cc3939786f441
boris.brinza <boris.brinza@aevi.cz>
2017-12-04 20:55:21 +0100

    aoc 2017 day 2

--- AdventOfCode2017/src/main/java/sk/mysko/aoc2017/Day1.java
+++ AdventOfCode2017/src/main/java/sk/mysko/aoc2017/Day1.java
@@ -7,9 +7,10 @@ public class Day1 extends AdventOfCodeBase<Integer> {
 
 	public static void main(String[] args) {
 		Day1 day1 = new Day1();
-		String input = day1.readFile("/Day1.input");
-		System.out.println("result:" + day1.runPart1(input));
-
+//		String input = day1.readFile("/Day1.input");
+//		System.out.println("result:" + day1.runPart1(input));
+		String input = day1.readFile("/Day1b.input");
+		System.out.println("result:" + day1.runPart2(input));
 
 
 	}
@@ -26,6 +27,12 @@ public class Day1 extends AdventOfCodeBase<Integer> {
 
 	@Override
 	protected Integer runPart2(String input) {
-		return null;
+		int sum = 0;
+		int diff = input.length() / 2;
+		for (int i = 0; i < input.length(); i++) {
+			int next = (i + diff) % input.length();
+			sum += (input.charAt(i) == input.charAt(next) ? (input.charAt(i) - '0') : 0);
+		}
+		return sum;
 	}
 }
---
+++ AdventOfCode2017/src/main/java/sk/mysko/aoc2017/Day2.java
@@ -0,0 +1,52 @@
+package sk.mysko.aoc2017;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author boris.brinza 04-Dec-2017.
+ */
+public class Day2 extends AdventOfCodeBase<Integer> {
+
+	public static void main(String[] args) {
+		Day2 day2 = new Day2();
+		String input = day2.readFile("/Day2.input");
+		System.out.println("result:" + day2.runPart1(input));
+		input = day2.readFile("/Day2.input");
+		System.out.println("result:" + day2.runPart2(input));
+
+
+	}
+
+	@Override
+	protected Integer runPart1(String input) {
+		int sum = 0;
+		String[] lines = input.split("\n");
+		for (String line : lines) {
+			IntSummaryStatistics s = Stream.of(line.split("\t")).collect(Collectors.summarizingInt(Integer::parseInt));
+			sum += (s.getMax() - s.getMin());
+		}
+		return sum;
+	}
+
+	@Override
+	protected Integer runPart2(String input) {
+		int sum = 0;
+		String[] lines = input.split("\n");
+		for (String line : lines) {
+			List<Integer> list = Stream.of(line.split("\t")).map(Integer::parseInt).sorted().collect(Collectors.toList());
+			for (int i = list.size() - 1; i > 1; i--) {
+				for (int j = i - 1; j > 0; j--) {
+					if (list.get(i) % list.get(j) == 0) {
+						sum += list.get(i) / list.get(j);
+						break;
+					}
+				}
+			}
+		}
+		return sum;
+	}
+}
