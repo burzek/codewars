cff51293be92cdd522bf11f65b15b1d36e01fa22
Boris Brinza <boris.brinza@aevi.cz>
2017-12-23 17:08:30 +0100

    advent of code 2017 day 20

---
+++ AdventOfCode2017/src/main/java/sk/mysko/aoc2017/Day21.java
@@ -0,0 +1,32 @@
+package sk.mysko.aoc2017;
+
+/**
+ * @author boris.brinza 04-Dec-2017.
+ */
+public class Day21 extends AdventOfCodeBase<Integer> {
+
+	public static void main(String[] args) {
+		Day21 day21 = new Day21();
+		String input = day21.readFile("/Day21.input");
+		System.out.println(day21.runPart1(input));
+		System.out.println(day21.runPart2(input));
+
+	}
+
+
+	@Override
+	protected Integer runPart1(String input) {
+		return 0;
+	}
+
+
+	@Override
+	protected Integer runPart2(String input) {
+		return 0;
+
+	}
+
+
+
+
+}
