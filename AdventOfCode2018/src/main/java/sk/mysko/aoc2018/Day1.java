70461b632c315865845537c92cc558eb5d09f987
boris.brinza <boris.brinza@aevi.cz>
2018-12-02 17:07:35 +0100

    day 2

---
+++ AdventOfCode2018/src/main/java/sk/mysko/aoc2018/Day1.java
@@ -0,0 +1,43 @@
+package sk.mysko.aoc2018;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author boris.brinza 04-Dec-2017.
+ */
+public class Day1 extends AdventOfCodeBase<Integer> {
+
+	public static void main(String[] args) {
+		Day1 day1 = new Day1();
+		String input = day1.readFile("/Day1.input");
+		System.out.println("result:" + day1.runPart1(input));
+		System.out.println("result:" + day1.runPart2(input));
+
+
+	}
+
+	@Override
+	protected Integer runPart1(String input) {
+		return Arrays.stream(input.split("\n")).mapToInt(Integer::parseInt).sum();
+	}
+
+	@Override
+	protected Integer runPart2(String input) {
+		Set<Integer> frequencies = new HashSet<>();
+		int sum = 0;
+		int f[] = Arrays.stream(input.split("\n")).mapToInt(Integer::parseInt).toArray();
+		int pos = 0;
+		do {
+			sum += f[pos++];
+			if (frequencies.contains(sum)) {
+				break;
+			}
+			frequencies.add(sum);
+			pos = pos % f.length;
+		} while (true);
+		return sum;
+	}
+}
