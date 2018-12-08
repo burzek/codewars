6a45baf63cf6761b079a3ca44d04a71ba849d27c
boris.brinza <boris.brinza@aevi.cz>
2018-12-08 15:38:40 +0100

    day 8

---
+++ AdventOfCode2018/src/main/java/sk/mysko/aoc2018/Day8.java
@@ -0,0 +1,90 @@
+package sk.mysko.aoc2018;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
+/**
+ * @author boris.brinza 07-dec-2018.
+ */
+public class Day8 extends AdventOfCodeBase<Integer> {
+
+	private class Node {
+		private List<Integer> metadata = new ArrayList<>();
+		private List<Node> children = new ArrayList<>();
+	}
+
+	private Node rootNode;
+
+	private int parse(String[] entries, int index, Node parentNode) {
+		Node node = new Node();
+		if (parentNode == null) {
+			rootNode = node;
+		} else {
+			parentNode.children.add(node);
+		}
+		int childrenCount = Integer.parseInt(entries[index++]);
+		int metaDataCount = Integer.parseInt(entries[index++]);
+		for (int i = 0; i < childrenCount; i++) {
+			index = parse(entries, index, node);
+		}
+		for (int i = 0; i < metaDataCount; i++) {
+			int md = Integer.parseInt(entries[index++]);
+			node.metadata.add(md);
+		}
+
+		return index;
+	}
+
+	private int sumMetadata(Node rootNode) {
+		int sum = rootNode.metadata.stream().mapToInt(i -> i).sum();
+		for (Node n : rootNode.children) {
+			sum += sumMetadata(n);
+		}
+		return sum;
+	}
+
+
+	private int getRootNodeValue(Node rootNode) {
+		if (rootNode.children.isEmpty()) {
+			return rootNode.metadata.stream().mapToInt(i -> i).sum();
+		} else {
+			int val = 0;
+			for (int md : rootNode.metadata) {
+				if (md <= rootNode.children.size()) {
+					val += getRootNodeValue(rootNode.children.get(rootNode.children.size() - md));
+				}
+			}
+			return val;
+		}
+
+	}
+
+	@Override
+	protected Integer runPart1(String input) {
+		String[] entries = input.split(" ");
+		parse(entries, 0, rootNode);
+		return sumMetadata(rootNode);
+	}
+
+	@Override
+	protected Integer runPart2(String input) {
+		String[] entries = input.split(" ");
+		parse(entries, 0, rootNode);
+		return getRootNodeValue(rootNode);
+	}
+
+
+
+	public static void main(String[] args) {
+		Day8 day = new Day8();
+		String input = day.readFile("/Day8.input");
+		System.out.println("result:" + day.runPart1(input));
+		System.out.println("result:" + day.runPart2(input));
+
+
+	}
+}
