53fca5a753b4a2adec3897c75b4e89c58e754908
Boris Brinza <boris.brinza@morosystems.cz>
2019-12-02 09:14:42 +0100

    aoc2019

---
+++ AdventOfCode2019/src/main/java/sk/mysko/aoc2019/AdventOfCodeBase.java
@@ -0,0 +1,31 @@
+package sk.mysko.aoc2019;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author boris.brinza
+ */
+public abstract class AdventOfCodeBase<T> {
+
+	protected  String readFile(String path) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(AdventOfCodeBase.class.getResourceAsStream(path)));
+		String s = null;
+		StringBuilder ret = new StringBuilder();
+		try {
+			while ((s = reader.readLine()) != null) {
+				ret.append(s).append("\n");
+			}
+		} catch (Exception e) {
+			System.err.println("Cannot read file:" + path + ", error:" + e.toString());
+			return null;
+		}
+		//remove last /n
+
+		return  ret.toString().substring(0, ret.length() - 1);
+
+	}
+
+
+
+}
---
+++ AdventOfCode2019/src/main/java/sk/mysko/aoc2019/Day1.java
@@ -0,0 +1,65 @@
+package sk.mysko.aoc2019;
+
+import java.util.Arrays;
+
+/**
+ * @author boris.brinza 04-Dec-2017.
+ */
+public class Day1 extends AdventOfCodeBase<Integer> {
+
+	public static void main(String[] args) {
+		Day1 day1 = new Day1();
+		String input = day1.readFile("/Day1.input");
+		System.out.println("result:" + day1.runPart1(input));
+		System.out.println("result:" + day1.runPart2(input));
+
+
+	}
+
+	protected Integer runPart1(String input) {
+		Integer[] program = Arrays.stream(input.split(",")).map(Integer::parseInt).toArray(Integer[]::new);
+		program[1] = 12;
+		program[2] = 2;
+		int pos = 0;
+		while (program[pos] != 99) {
+			int op = program[pos];
+			if (op == 1) {
+				program[program[pos + 3]] = program[program[pos + 1]] + program[program[pos + 2]];
+			} else if (op == 2) {
+				program[program[pos + 3]] = program[program[pos + 1]] * program[program[pos + 2]];
+			}
+			pos += 4;
+		}
+		return program[0];
+	}
+
+	protected Integer runPart2(String input) {
+		Integer[] program;
+		int verb = 0;
+		int noun = 0;
+		for (;;) {
+			program = Arrays.stream(input.split(",")).map(Integer::parseInt).toArray(Integer[]::new);
+			program[1] = noun;
+			program[2] = verb;
+			int pos = 0;
+			while (program[pos] != 99) {
+				int op = program[pos];
+				if (op == 1) {
+					program[program[pos + 3]] = program[program[pos + 1]] + program[program[pos + 2]];
+				} else if (op == 2) {
+					program[program[pos + 3]] = program[program[pos + 1]] * program[program[pos + 2]];
+				}
+				pos += 4;
+			}
+			if (program[0] == 19690720) {
+				break;
+			}
+			verb++;
+			if (verb == 100) {
+				verb = 0;
+				noun++;
+			}
+		}
+		return noun * 100 + verb;
+	}
+}
