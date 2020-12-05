c478c711203eb1c823168df3dcd5686f643380cb
Boris Brinza <boris.brinza@morosystems.cz>
2020-12-05 11:22:35 +0100

    fix day 5

--- AdventOfCode2020/src/main/java/Day5.java
+++ AdventOfCode2020/src/main/java/Day5.java
@@ -1,49 +1,59 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author boris.brinza 01.dec.2020
  */
-public class Day5 extends AdventOfCodeBase<Long> {
+public class Day5 extends AdventOfCodeBase<Void> {
 
     public static void main(String[] args) {
         Day5 day5 = new Day5();
         String input = day5.readFile("/Day5.input");
         String[] lines = input.split("\n");
-        System.out.println("part1:" + day5.runSolution(lines));
+        day5.runSolution(lines);
 
     }
 
-    private long runSolution(String[] lines) {
+    private int getSeat(char highMark, int maxVal, String def) {
+        int min = 0;
+        int max = maxVal;
+        for (char c : def.toCharArray()) {
+            if (c == highMark) {
+                max = min + (max - min) / 2;
+
+            } else {
+                min = min + (max - min) / 2 + 1;
+            }
+        }
+        return min; //same as max
+    }
+
+    private void runSolution(String[] lines) {
         int maxSeat = 0;
+        List<Integer> seats = new ArrayList<>();
+
         for (String line : lines) {
             String rowDef = line.substring(0, 7);
             String colDef = line.substring(7);
+            int seat = getSeat('F', 127, rowDef) * 8 + getSeat('L', 7, colDef);
 
-            int min = 0;
-            int max = 127;
-            for (char c : rowDef.toCharArray()) {
-                if (c == 'F') {
-                    max = min + (max - min) / 2;
-
-                } else if (c == 'B') {
-                    min = min + (max - min) / 2 + 1;
-                }
-            }
-
-            int minR = 0;
-            int maxR = 7;
-            for (char c : colDef.toCharArray()) {
-                if (c == 'L') {
-                    maxR = minR + (maxR - minR) / 2;
-
-                } else if (c == 'R') {
-                    minR = minR + (maxR - minR) / 2 + 1;
-                }
-            }
-
-            maxSeat = Math.max(maxSeat, min * 8 + minR);
+            maxSeat = Math.max(maxSeat, seat);
+            seats.add(seat);
 
         }
 
-        return maxSeat;
+        Collections.sort(seats);
+        int mySeat = 0;
+        for (int i = 0; i < seats.size() - 1; i++) {
+            if (seats.get(i + 1) - seats.get(i) == 2) {
+                mySeat = seats.get(i) + 1;
+                break;
+            }
+        }
+
+        System.out.println("part1:" + maxSeat);
+        System.out.println("part2:" + mySeat);
 
     }
 
