9dc9961bc9a765428799092540d666f3e9974b2e
boris <boris.brinza@morosystems.cz>
2022-12-13 13:20:22 +0100

    day12 part 2

--- AdventOfCode2022/day12.cpp
+++ AdventOfCode2022/day12.cpp
@@ -12,16 +12,16 @@ using namespace std;
 
 
 void add_edge(vector<unsigned> adj[], unsigned src, unsigned dest, char src_val, char dest_val) {
-    if (src_val >= dest_val || src_val == dest_val - 1) {
+    if (src_val <= dest_val || src_val == dest_val + 1) {
         adj[src].push_back(dest);
     }
-    if (dest_val >= src_val || dest_val == src_val - 1) {
+    if (dest_val <= src_val || dest_val == src_val + 1) {
         adj[dest].push_back(src);
     }
 }
 
 
-unsigned BFS(vector<unsigned> adj[], unsigned src, unsigned dest, unsigned v) {
+void BFS(vector<unsigned> adj[], vector<char> start_points, unsigned src, unsigned dest, unsigned v) {
     unsigned dist[v];
     bool visited[v];
     list<unsigned> queue;
@@ -43,24 +43,32 @@ unsigned BFS(vector<unsigned> adj[], unsigned src, unsigned dest, unsigned v) {
                 visited[adj[u][i]] = true;
                 dist[adj[u][i]] = dist[u] + 1;
                 queue.push_back(adj[u][i]);
-                 if (adj[u][i] == dest) {
-                    return dist[dest];
-                }
             }
         }
     }
-    return 0;
+
+    //min dist
+    unsigned min = INT_MAX;
+    for (int i = 0; i < start_points.size(); i++) {
+        if (start_points[i] == 'a' && dist[i] < min) {
+            min = dist[i];
+        }
+    }
+    cout << "Part1: " << dist[dest] << endl;
+    cout << "Part2: " << min << endl;
+
 }
 
 void read(string inputFileName,
                     vector<unsigned> adj[],
+                    vector<char> &start_points,
                     unsigned rows, unsigned cols,
                     unsigned& start_vertex, unsigned& end_vertex) {
 
     ifstream file(inputFileName);
     string line;
 
-    getline(file, line); //read first line
+    getline(file, line);
     start_vertex = -1;
     end_vertex = -1;
     
@@ -69,15 +77,16 @@ void read(string inputFileName,
         getline(file, line_p_1);
 
         for (int c = 0; c < cols; c++) {
-            int vertex_index = r * cols + c;
+            int vertex_index = r * cols + c;        
+            //search from E-S (its better for part 2)
             if (start_vertex == -1) {
-                start_vertex = (line[c] == 'S' ? vertex_index : -1);
+                start_vertex = (line[c] == 'E' ? vertex_index : -1);
             }
             if (end_vertex == -1) {
-                end_vertex = (line[c] == 'E' ? vertex_index : -1);
+                end_vertex = (line[c] == 'S' ? vertex_index : -1);  
             }
-            line[c] = (line[c] == 'S' ? 'a' : line[c]);
-            line[c] = (line[c] == 'E' ? 'z' : line[c]);
+            line[c] = (line[c] == 'S' ? 'a' : line[c] == 'E' ? 'z' : line[c]);
+            start_points.push_back(line[c]);
 
             if (c > 0) {
                 add_edge(adj, vertex_index - 1 , vertex_index, line.at(c - 1), line.at(c));
@@ -88,18 +97,16 @@ void read(string inputFileName,
         }
         line = line_p_1;
     }
-   
+
 }
 
 void solve(string file_name, int rows, int cols) {
     unsigned start_index;
     unsigned end_index;
     vector<unsigned> adj[rows * cols];
-    read(file_name, adj, rows, cols, start_index, end_index);
-    unsigned len = BFS(adj, start_index, end_index, cols * rows);
-    cout << "Part1: " << len << endl;
-
-
+    vector<char> start_points;
+    read(file_name, adj, start_points, rows, cols, start_index, end_index);
+    BFS(adj, start_points, start_index, end_index, cols * rows);
 }
 
 
