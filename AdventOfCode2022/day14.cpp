e85a9c30a16a49c09ca8b25e43f50ec9b65cadfb
boris <boris.brinza@morosystems.cz>
2022-12-14 22:17:46 +0100

    day14

--- AdventOfCode2022/day14.cpp
+++ AdventOfCode2022/day14.cpp
@@ -13,42 +13,15 @@ using namespace std;
 #define AIR '.'
 #define WALL '#'
 #define ROCK 'o'
-#define TRACE 'x'
 
-//160
-//#define X_DIFF 490
-#define X_DIFF 160
+#define MAP_SIZE 600
+#define COL_SHIFT 300
 
-void clear_trace(unsigned char map[][200]) {
-    for (int x = 0; x < 20; x++) {
-        for (int y = 0; y < 20; y++) {
-            if (map[y][x] == 'x') {
-                map[y][x] = '.';
-            }
-        }
-    }
-
-}
-
-
-void print_map(unsigned char map[][200], int fy) {
-    for (int r = fy - 10; r < fy + 10; r++) {
-        for (int c = 0; c < 150; c++) {
-            cout << (map[r][c]);
-        }
-    cout << endl;
-    }
-    cout << "-------------------------------------------" << endl;
-
-}
-
-void solve(string file_name) {
+int read_map(string file_name, char (&map)[][MAP_SIZE]) {
     ifstream file(file_name);
     string line;
 
-    unsigned char map[200][200];
-    memset(map, AIR, sizeof(map[0][0]) * 200 * 200);
-
+    memset(map, AIR, sizeof(map[0][0]) * MAP_SIZE * MAP_SIZE);
     
     int max_r = 0;
     while (getline(file, line)) {
@@ -58,54 +31,52 @@ void solve(string file_name) {
         points.erase(remove_if(points.begin(), points.end(), [](string x){return x.length() == 0;}), points.end());
         int from_r {0}, from_c {0}, to_r {0}, to_c {0};
         
-        for (int pi = 0; pi < points.size() - 1; pi++) {
+        for (int pi = {0}; pi < points.size() - 1; pi++) {
             from_c = atoi(points[pi].substr(0, points[pi].find_first_of(',')).c_str());
             from_r = atoi(points[pi].substr(points[pi].find_first_of(',') + 1).c_str());
             to_c = atoi(points[pi + 1].substr(0, points[pi + 1].find_first_of(',')).c_str());
             to_r = atoi(points[pi + 1].substr(points[pi + 1].find_first_of(',') + 1).c_str());
+            max_r = max(to_r, max_r);
             from_c > to_c ? swap(from_c, to_c) : void(0);
             from_r > to_r ? swap(from_r, to_r) : void(0);
-            max_r = max(to_r, max_r);
             for (int r = from_r; r <= to_r; r++) {
                 for (int c = from_c; c <= to_c; c++) {
-                    map[r][c - X_DIFF] = WALL;
+                    map[r][c - COL_SHIFT] = WALL;
                 }
             }
         }
     }
 
-
-    print_map(map, max_r);
-    for (int x = 0; x < 200; x++) {
-     ///   map[max_r + 2][x] = WALL;
+    //add bottom line
+    for (int c = {0}; c < MAP_SIZE; c++) {
+        map[max_r + 2][c] = WALL;
     }
-    cout << endl << endl;
-    print_map(map, max_r);
 
-    
-    int rock_count_to_touch_floor = 0;
-    int rock_count_to_source = 0;
+    return max_r;
+}
+
+void solve(string file_name) {
+    char map[MAP_SIZE][MAP_SIZE];
+    int max_r = read_map(file_name, map);
+
+    int rock_count_to_touch_floor = {0};
+    int rock_count_to_source = {0};
 
     //simulate
     int rock_count = 0;
     while (rock_count_to_source == 0 || rock_count_to_touch_floor == 0) {
         rock_count++;
-        int c = 500 - X_DIFF;
-        int r = 0;
+        int c = {500 - COL_SHIFT};
+        int r = {0};
         bool falling = true;
+        if (map[r][c] == ROCK) {
+            rock_count_to_source = rock_count - 1;
+            falling = false;
+        } 
         while (falling) {
-            // if (map[0][x] == ROCK) {
-            //     rock_count_to_source = rock_count - 1;
-            //     falling = false;
-            // } 
-
-            if (r == max_r) {
-                if (rock_count_to_touch_floor == 0) {
-                    rock_count_to_touch_floor = rock_count - 1;
-                    cout << "PART1:" << rock_count_to_touch_floor << endl;
-                }
+            if (r >= max_r && rock_count_to_touch_floor == 0) {
+                rock_count_to_touch_floor = rock_count - 1;
             }
-
             if (map[r + 1][c] != WALL && map[r + 1][c] != ROCK) {
                 r++;
             } else if (c > 0 && map[r + 1][c - 1] != WALL && map[r + 1][c - 1] != ROCK) {
@@ -116,12 +87,7 @@ void solve(string file_name) {
                 falling = false;
                 map[r][c] = ROCK;
             }
-
-            //cout << "y=" << y << ", max_y=" << max_y << endl;
-            //print_map(map);
         }
-
-        //clear_trace(map);
     }
     
     cout << "Part1:" << rock_count_to_touch_floor << endl;
