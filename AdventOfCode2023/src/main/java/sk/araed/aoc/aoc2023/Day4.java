ffeff6dd0ea40e3b6221270d58015f8b3daf89b7
Boris Brinza <brinza@essential-data.sk>
2023-12-04 14:56:51 +0100

    d4

--- AdventOfCode2023/src/main/java/sk/araed/aoc/aoc2023/Day4.java
+++ AdventOfCode2023/src/main/java/sk/araed/aoc/aoc2023/Day4.java
@@ -2,6 +2,8 @@ package sk.araed.aoc.aoc2023;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.IntUnaryOperator;
@@ -9,22 +11,24 @@ import java.util.stream.Collectors;
 
 public class Day4 {
 
+  private final List<Set<Integer>> winningCards = new ArrayList<>();
+
   public static void main(String[] args) {
     Day4 day4 = new Day4();
     String[] input = AocHelper.readInputToLines("/day4.input");
 
+    day4.prepare(input);
     System.out.println("day4(1) = " + day4.runPart1(input));
     System.out.println("day4(2) = " + day4.runPart2(input));
   }
 
   private long runPart1(final String[] input) {
     long sum = 0L;
-    for (final String line : input) {
-      Set<Integer> myWinCards = getWinningCards(line);
+    for (int lineNo = 0; lineNo < input.length; lineNo++) {
+      Set<Integer> myWinCards = winningCards.get(lineNo);
       if (!myWinCards.isEmpty()) {
         sum += ((long) 1 << myWinCards.size() - 1);
       }
-
     }
     return sum;
   }
@@ -34,7 +38,7 @@ public class Day4 {
     Arrays.fill(copies, 1);
 
     for (int currentCard = 0; currentCard < copies.length; currentCard++) {
-      for (int w = 0; w < getWinningCards(input[currentCard]).size(); w++) {
+      for (int w = 0; w < winningCards.get(currentCard).size(); w++) {
         copies[currentCard + w + 1] += copies[currentCard];
       }
     }
@@ -42,21 +46,24 @@ public class Day4 {
 
   }
 
-  private Set<Integer> getWinningCards(String line) {
-    Set<Integer> winning = Arrays
-        .stream(line.substring(line.indexOf(':') + 1, line.indexOf('|'))
-            .split(" "))
-        .filter(s -> !s.isEmpty())
-        .map(s -> Integer.parseInt(s.trim()))
-        .collect(Collectors.toSet());
-    Set<Integer> owning = Arrays
-        .stream(line.substring(line.indexOf('|') + 2)
-            .split(" "))
-        .filter(s -> !s.isEmpty())
-        .map(s -> Integer.parseInt(s.trim()))
-        .collect(Collectors.toSet());
-    owning.retainAll(winning);
-    return owning;
+  private void prepare(final String[] input) {
+    for (final String line : input) {
+      Set<Integer> winningCards = Arrays
+          .stream(line.substring(line.indexOf(':') + 1, line.indexOf('|'))
+              .split(" "))
+          .filter(s -> !s.isEmpty())
+          .map(s -> Integer.parseInt(s.trim()))
+          .collect(Collectors.toSet());
+      Set<Integer> owningCards = Arrays
+          .stream(line.substring(line.indexOf('|') + 2)
+              .split(" "))
+          .filter(s -> !s.isEmpty())
+          .map(s -> Integer.parseInt(s.trim()))
+          .collect(Collectors.toSet());
+      owningCards.retainAll(winningCards);
+      this.winningCards.add(owningCards);
+    }
   }
+
 
 }
